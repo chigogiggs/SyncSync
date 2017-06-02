6de9360e0fe1f234caface8f7fbbbfd7b7ea4e44
chigo anyaso <chigoanyaso@Melissas-MacBook-Pro.local>
2017-06-01 20:00:36 -0400

    added chromerdriver and stuff

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import selenium
+import subprocess
+from subprocess import call
 from selenium.webdriver.common.action_chains import ActionChains
+from subprocess import call
 
+tempvol = 0
+volume = 0
 
-
+call(["amixer", "-D", "pulse", "sset", "Master", "10%+"])
 
 config = {
   "apiKey": "AIzaSyAujDNI5-U7PUG-jtB0jUZsklX2mXPkjSs",
@@ -42,9 +47,38 @@ wait = WebDriverWait(driver, 15)
 driver.get(url)
 last_link = url
 
+def get_master_volume():
+    proc = subprocess.Popen('/usr/bin/amixer sget Master', shell=True, stdout=subprocess.PIPE)
+    amixer_stdout = proc.communicate()[0].split('\n')[4]
+    proc.wait()
+
+    find_start = amixer_stdout.find('[') + 1
+    find_end = amixer_stdout.find('%]', find_start)
+
+    return float(amixer_stdout[find_start:find_end])
+print(get_master_volume())
+def increasevolume(num):
+    call(["amixer", "-D", "pulse", "sset", "Master", str(num)+"%"])
+def decreasevoume(num):
+    call(["amixer", "-D", "pulse", "sset", "Master", str(num)+"0%"])
+
 while 1:
     info = db.child("youtube").get()
     url = info.val()['link']
+    volinfo = db.child("volume").get()
+    vol  = int(info.val()['vol'])
+
+    if int(vol) > tempvol:
+        if volume < 100:
+            volume += vol
+            increasevolume(volume)
+
+    elif int(vol) < tempvol:
+        if volume > 0 :
+            volume -= vol
+            decreasevoume(volume)
+
+    tempvol = int(vol)
 
     if url != last_link:
         driver.get(url)
